da40e773b5c51878131029c645e75e6ca5741982
Majorfi <major-tom.eth@pm.me>
2022-06-21 17:56:28 +0200

    fix: add github webhook handler

--- go.mod
+++ go.mod
@@ -24,6 +24,7 @@ require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
+	github.com/go-playground/webhooks/v6 v6.0.1 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/google/uuid v1.3.0 // indirect
--- internal/controllers/routes.go
+++ internal/controllers/routes.go
@@ -7,11 +7,13 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/webhooks/v6/github"
 	"github.com/machinebox/graphql"
 	"github.com/majorfi/ydaemon/internal/ethereum"
 	"github.com/majorfi/ydaemon/internal/logs"
 	"github.com/majorfi/ydaemon/internal/models"
 	"github.com/majorfi/ydaemon/internal/store"
+	"github.com/majorfi/ydaemon/internal/utils"
 )
 
 type controller struct{}
@@ -90,8 +92,16 @@ func (y controller) GetAllVaults(c *gin.Context) {
 
 //TriggerWebhook will do trigger a webhook from github
 func (y controller) TriggerWebhook(c *gin.Context) {
-	logs.Pretty(c.Params)
-	logs.Pretty(c.GetRawData())
+	hook, _ := github.New(github.Options.Secret(utils.WebhookSecret))
+
+	payload, err := hook.Parse(c.Request, github.DeploymentEvent, github.DeploymentStatusEvent)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	logs.Pretty(payload)
 
 	c.JSON(http.StatusOK, "OK")
 }

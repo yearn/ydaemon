e8518bf178fe46288c1ac75280b4eb1f69120f4b
borrito <yh01dlx@gmail.com>
2022-09-29 17:17:41 +0200

    refactor: remove unused mutex fn

--- internal/strategies/utils.go
+++ internal/strategies/utils.go
@@ -1,25 +1,3 @@
 package strategies
 
-import (
-	"sync"
-
-	"github.com/yearn/ydaemon/internal/utils/models"
-)
-
 type Controller struct{}
-
-func GetStrategyGroupsFromStrategy(strat models.TStrategyList, groups []*TStrategyGroupFromRisk) []*TStrategyGroupFromRisk {
-	var stratGroups []*TStrategyGroupFromRisk
-	var mutex = &sync.Mutex{}
-	mutex.Lock()
-	for _, group := range groups {
-		if excludeNameLike(strat, *group) {
-			continue
-		}
-		if includeAddress(strat, *group) || includeNameLike(strat, *group) {
-			stratGroups = append(stratGroups, group)
-		}
-	}
-	mutex.Unlock()
-	return stratGroups
-}

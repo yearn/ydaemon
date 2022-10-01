378ecf30943ef63a5ce7e483a32369186f70a295
Majorfi <major-tom.eth@pm.me>
2022-10-01 12:05:22 +0200

    fix: all chains

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,14 @@ import (
 	"strconv"
 	"sync"
 
+	"github.com/yearn/ydaemon/internal/utils/env"
 	"github.com/yearn/ydaemon/internal/utils/logs"
 	"github.com/yearn/ydaemon/internal/utils/store"
 )
 
-// var chains = helpers.SUPPORTED_CHAIN_IDS
-var chains = []uint64{1}
+var chains = env.SUPPORTED_CHAIN_IDS
+
+// var chains = []uint64{1}
 
 func waitGroupSummonDaemons(wg *sync.WaitGroup, chainID uint64) {
 	SummonDaemons(chainID)

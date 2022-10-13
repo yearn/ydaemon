7f1b104726c5a24afd6277a7452c56d396382e1c
Majorfi <major-tom.eth@pm.me>
2022-10-13 23:25:24 +0200

    feat: add RetrieveAllStrategies process

--- internal/utils/events.go
+++ internal/utils/events.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/yearn/ydaemon/common/bigNumber"
 )
 
 type TVaultsFromRegistry struct {
@@ -16,4 +17,29 @@ type TVaultsFromRegistry struct {
 	ManagementFee   uint64
 	TxIndex         uint
 	LogIndex        uint
+}
+
+type TStrategyAdded struct {
+	VaultAddress      common.Address
+	StrategyAddress   common.Address
+	TxHash            common.Hash
+	DebtRatio         *bigNumber.Int // >= 0.3.0
+	MaxDebtPerHarvest *bigNumber.Int // >= 0.3.2
+	MinDebtPerHarvest *bigNumber.Int // >= 0.3.2
+	PerformanceFee    *bigNumber.Int // >= 0.2.2
+	DebtLimit         *bigNumber.Int // == 0.2.2
+	RateLimit         *bigNumber.Int // == 0.2.2 - 0.3.0
+	BlockNumber       uint64
+	TxIndex           uint
+	LogIndex          uint
+}
+
+type TStrategyMigrated struct {
+	VaultAddress       common.Address
+	OldStrategyAddress common.Address
+	NewStrategyAddress common.Address
+	TxHash             common.Hash
+	BlockNumber        uint64
+	TxIndex            uint
+	LogIndex           uint
 }

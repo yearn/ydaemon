ba35dd9cfa75c3de5fa046a2ccfdba306e0061c3
Majorfi <major-tom.eth@pm.me>
2022-10-18 14:18:12 +0200

    fix: linting

--- docs/pages/_document.tsx
+++ docs/pages/_document.tsx
@@ -1,27 +1,52 @@
 import React, {ReactElement} from 'react';
-import Document, {Html, Head, Main, NextScript, DocumentContext} from 'next/document';
+import Document, {DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript} from 'next/document';
 import {SkipNavLink} from '@reach/skip-nav';
 
-type TInitialProps = {
-    html: string;
-    head?: (JSX.Element | null)[] | undefined;
-    styles?: React.ReactElement[] | React.ReactFragment | undefined;
-}
+const modeScript = `
+  let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+
+  updateMode()
+  darkModeMediaQuery.addEventListener('change', updateModeWithoutTransitions)
+  window.addEventListener('storage', updateModeWithoutTransitions)
+
+  function updateMode() {
+    let isSystemDarkMode = darkModeMediaQuery.matches
+    let isDarkMode = true
+
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+
+    if (isDarkMode === isSystemDarkMode) {
+      delete window.localStorage.isDarkMode
+    }
+  }
+
+  function updateModeWithoutTransitions() {
+    updateMode()
+  }
+`;
 
 class MyDocument extends Document {
-	static async getInitialProps(ctx: DocumentContext): Promise<TInitialProps> {
+	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
 		const initialProps = await Document.getInitialProps(ctx);
-		return {...initialProps} as any; // eslint-disable-line
+		return {...initialProps};
 	}
+
 	render(): ReactElement {
 		return (
 			<Html lang={'en'} className={'dark'}>
 				<Head>
-					<link rel={'preconnect'} href={'https://fonts.googleapis.com'} />
-					<link rel={'preconnect'} href={'https://fonts.gstatic.com'} crossOrigin={'true'} />
-					<link href={'https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&family=Roboto:wght@400;700&display=swap'} rel={'stylesheet'} />
+					<link
+						rel={'preconnect'}
+						href={'https://brand.yearn.finance'}
+						crossOrigin={'true'} />
+					<link href={'https://brand.yearn.finance/fonts/fonts.css'} rel={'stylesheet'} />
+					<script dangerouslySetInnerHTML={{__html: modeScript}} />
 				</Head>
-				<body className={'bg-neutral-200 transition-colors duration-150'} data-theme={'light'}>
+				<body className={'bg-neutral-200 transition-colors duration-150'} data-theme={'dark'}>
 					<SkipNavLink />
 					<Main />
 					<NextScript />

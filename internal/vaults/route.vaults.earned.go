5158984cf302aa05523e222ce84e255b136402e4
Majorfi <major-tom.eth@pm.me>
2022-11-07 14:56:13 +0100

    feat: add some comments

--- internal/vaults/route.vaults.earned.go
+++ internal/vaults/route.vaults.earned.go
@@ -53,44 +53,79 @@ func (y Controller) GetEarnedPerVaultPerUser(c *gin.Context) {
 	pricePerShare := bigNumber.NewInt(0).SetString(response.AccountVaultPositions[0].Vault.LatestUpdate.PricePerShare)
 	decimals := bigNumber.NewInt(response.AccountVaultPositions[0].Vault.ShareToken.Decimals)
 	decimals = bigNumber.NewInt(0).Exp(bigNumber.NewInt(10), decimals, nil)
-	logs.Pretty(decimals)
 	decimalsFloat := bigNumber.NewFloat(0).SetInt(decimals)
-	sumOfDeposits := bigNumber.NewInt(0)
-	sumOfWithdrawals := bigNumber.NewInt(0)
-	sumOfSharesBurned := bigNumber.NewInt(0)
-	sumOfSharesMinted := bigNumber.NewInt(0)
 	allDepositsPricePerShare := []*bigNumber.Float{}
 	allWithdrawalsPricePerShare := []*bigNumber.Float{}
 	allShareMinted := []*bigNumber.Int{}
 	allShareBurned := []*bigNumber.Int{}
 
+	/**************************************************************************************************
+	** Processing all the updates to get:
+	** - a list of all deposits price per share
+	** - a list of all withdrawals price per share
+	** - a list of all share minted
+	** - a list of all share burned
+	** We will have as many elements in depositsPricePerShare as in sharesMinted and as many elements
+	** in withdrawalsPricePerShare as in sharesBurned, which will allow us to find, for a given index
+	** in sharesMinted, the corresponding deposit price per share.
+	**************************************************************************************************/
 	for _, update := range allUpdates {
 		deposit := bigNumber.NewInt().SetString(update.Deposits)
 		withdrawal := bigNumber.NewInt().SetString(update.Withdrawals)
 		sharesBurnt := bigNumber.NewInt().SetString(update.SharesBurnt)
 		sharesMinted := bigNumber.NewInt().SetString(update.SharesMinted)
+		sharesReceived := bigNumber.NewInt().SetString(update.SharesReceived)
+		sharesSent := bigNumber.NewInt().SetString(update.SharesSent)
 
 		if deposit != nil && !deposit.IsZero() {
 			pricePerShareAtThisTime := bigNumber.NewFloat(0).Div(bigNumber.NewFloat().SetInt(deposit), bigNumber.NewFloat().SetInt(sharesMinted))
-			logs.Info(`user deposited`, deposit, `shares minted`, sharesMinted, `price per share`, pricePerShareAtThisTime)
+			allDepositsPricePerShare = append(allDepositsPricePerShare, pricePerShareAtThisTime)
+			allShareMinted = append(allShareMinted, sharesMinted)
+		}
+		if sharesReceived != nil && !sharesReceived.IsZero() {
+			pricePerShareAtThisTime := bigNumber.NewFloat(0).Div(bigNumber.NewFloat().SetInt(sharesReceived), bigNumber.NewFloat().SetInt(sharesMinted))
 			allDepositsPricePerShare = append(allDepositsPricePerShare, pricePerShareAtThisTime)
 			allShareMinted = append(allShareMinted, sharesMinted)
 		}
 		if withdrawal != nil && !withdrawal.IsZero() {
 			pricePerShareAtThisTime := bigNumber.NewFloat(0).Div(bigNumber.NewFloat().SetInt(withdrawal), bigNumber.NewFloat().SetInt(sharesBurnt))
-			logs.Info(`user withdrew`, withdrawal, `shares burnt`, sharesBurnt, `price per share`, pricePerShareAtThisTime)
 			allWithdrawalsPricePerShare = append(allWithdrawalsPricePerShare, pricePerShareAtThisTime)
 			allShareBurned = append(allShareBurned, sharesBurnt)
 		}
-
-		sumOfDeposits.Add(sumOfDeposits, deposit)
-		sumOfWithdrawals.Add(sumOfWithdrawals, withdrawal)
-		sumOfSharesBurned.Add(sumOfSharesBurned, sharesBurnt)
-		sumOfSharesMinted.Add(sumOfSharesMinted, sharesMinted)
+		if sharesSent != nil && !sharesSent.IsZero() {
+			pricePerShareAtThisTime := bigNumber.NewFloat(0).Div(bigNumber.NewFloat().SetInt(sharesSent), bigNumber.NewFloat().SetInt(sharesBurnt))
+			allWithdrawalsPricePerShare = append(allWithdrawalsPricePerShare, pricePerShareAtThisTime)
+			allShareBurned = append(allShareBurned, sharesBurnt)
+		}
 	}
 
-	// exclude shares sent and received?
-
+	/**************************************************************************************************
+	** First step is to compute the realized gains or losses. Plan is to use the FIFO method, aka
+	** First In First Out. In order to do so, we will iterate over all withdrawals (allSharesBurned)
+	** and compare the amount of shares burned with the oldest shares minted. This oldest shares minted
+	** will act like if it was a queue, meaning that we will withdraw X shares burned from it, with it's
+	** own pricePerShare, until it is empty, then we will move to the next oldest shares minted and
+	** so on.
+	**
+	** Example:
+	**
+	** Shares minted: [100, 100] [pricePerShare: 1, 1.2]
+	** Shares burned: [50, 80] [pricePerShare: 1.5, 1.5]
+	**
+	** Step 1:
+	** - 50 shares burned from the first shares minted, which has a pricePerShare of 1
+	** - New Shares minted queue: [50, 100] [pricePerShare: 1, 1.2]
+	** - FIFO earned: 50 * 1.5 - 50 * 1 = 50 * 0.5 = 25
+	** - Total FIFO earned: 25
+	**
+	** Step 2:
+	** - 50 shares burned from the first shares minted, which has a pricePerShare of 1 (max possible)
+	** - 30 shares burned from the second shares minted, which has a pricePerShare of 1.2
+	** - New Shares minted queue: [0, 70] [pricePerShare: 1, 1.2]
+	** - FIFO earned: 50 * 1.5 - 50 * 1 = 50 * 0.5 = 25
+	** - FIFO earned: 30 * 1.5 - 30 * 1.2 = 30 * 0.3 = 9
+	** - Total FIFO earned: 25 + 9 = 34
+	**************************************************************************************************/
 	fifoRealizedGains := []*bigNumber.Float{}
 	latestMintIndex := 0
 	for index := 0; index < len(allShareBurned); {
@@ -163,7 +198,11 @@ func (y Controller) GetEarnedPerVaultPerUser(c *gin.Context) {
 		realizedGains.Add(realizedGains, gain)
 	}
 
-	// Calculate unrealizedGains
+	/**************************************************************************************************
+	** Second step is to do almost the same thing for the unrealized gains. We will proceed just like
+	** we did for the realized gains, but we will use the current price per share and all the
+	** remaining shares.
+	**************************************************************************************************/
 	fifoUnRealizedGains := []*bigNumber.Float{}
 	currentPPS := bigNumber.NewFloat(0).Div(bigNumber.NewFloat().SetInt(pricePerShare), decimalsFloat)
 	for index := range allShareMinted {
@@ -178,7 +217,6 @@ func (y Controller) GetEarnedPerVaultPerUser(c *gin.Context) {
 
 		fifoUnRealizedGains = append(fifoUnRealizedGains, bigNumber.NewFloat(0).Sub(scaledValue, mintedValue))
 	}
-
 	unrealizedGains := bigNumber.NewFloat(0)
 	for _, gain := range fifoUnRealizedGains {
 		unrealizedGains.Add(unrealizedGains, gain)

ac93e0f85f681e969aaaed2c6d9fb89c000ccf5b
mariuspod <14898268+mariuspod@users.noreply.github.com>
2022-11-17 12:54:35 +0100

    feat: replace repo and image urls.

--- deploy.sh
+++ deploy.sh
@@ -14,12 +14,12 @@ if ! [ -f "$ENV_FILE" ]; then
 fi
 
 rm -fr $YD_HOME || true
-git clone https://github.com/mariuspod/ydaemon $YD_HOME
+git clone https://github.com/yearn/ydaemon $YD_HOME
 cd $YD_HOME
 git checkout $BRANCH
 
 source $ENV_FILE
-docker pull "ghcr.io/mariuspod/ydaemon:$TAG"
+docker pull "ghcr.io/yearn/ydaemon:$TAG"
 docker-compose -f docker-compose.dev.yml stop
 docker-compose -f docker-compose.dev.yml up -d
 

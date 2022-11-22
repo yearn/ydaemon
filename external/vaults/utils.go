44556b9057701a76b3159dc650469f3aec05e0eb
Majorfi <major-tom.eth@pm.me>
2022-11-22 15:48:08 +0100

    fix: static check

--- external/vaults/utils.go
+++ external/vaults/utils.go
@@ -1,21 +1,15 @@
 package vaults
 
-import (
-	"strings"
-
-	"github.com/yearn/ydaemon/common/env"
-)
-
 type Controller struct{}
 
-func selectStrategiesCondition(s string) string {
-	if s == `` {
-		return `debtLimit`
-	}
-	for _, c := range env.STRATEGIES_CONDITIONS {
-		if strings.EqualFold(c, s) {
-			return c
-		}
-	}
-	return `debtLimit`
-}
+// func selectStrategiesCondition(s string) string {
+// 	if s == `` {
+// 		return `debtLimit`
+// 	}
+// 	for _, c := range env.STRATEGIES_CONDITIONS {
+// 		if strings.EqualFold(c, s) {
+// 			return c
+// 		}
+// 	}
+// 	return `debtLimit`
+// }

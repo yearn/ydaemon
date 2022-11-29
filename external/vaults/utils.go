da73a69e4549b612e8a5a29f8652dc91ef9f4b31
Majorfi <major-tom.eth@pm.me>
2022-11-29 17:10:25 +0100

    feat: re-enablee strategiesCondition

--- external/vaults/utils.go
+++ external/vaults/utils.go
@@ -1,15 +1,21 @@
 package vaults
 
+import (
+	"strings"
+
+	"github.com/yearn/ydaemon/common/env"
+)
+
 type Controller struct{}
 
-// func selectStrategiesCondition(s string) string {
-// 	if s == `` {
-// 		return `debtLimit`
-// 	}
-// 	for _, c := range env.STRATEGIES_CONDITIONS {
-// 		if strings.EqualFold(c, s) {
-// 			return c
-// 		}
-// 	}
-// 	return `debtLimit`
-// }
+func selectStrategiesCondition(s string) string {
+	if s == `` {
+		return `debtLimit`
+	}
+	for _, c := range env.STRATEGIES_CONDITIONS {
+		if strings.EqualFold(c, s) {
+			return c
+		}
+	}
+	return `debtLimit`
+}

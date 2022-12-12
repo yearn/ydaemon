f384d9544ca4958f3e30f9573649220a9a67e199
borrito <yh01dlx@gmail.com>
2022-12-12 15:41:39 +0100

    fix: listen to the static checks

--- common/env/constants.go
+++ common/env/constants.go
@@ -142,20 +142,24 @@ var CURVE_REGISTRY_ADDRESSES = map[uint64]common.Address{
 	42161: common.HexToAddress(`0x0000000022d53366457f9d5e68ec105046fc4383`),
 }
 
-// LLAMA_PRICE_URL contains the URL for the DeFiLlama pricing API
+// LLAMA_CHAIN_NAMES contains the chain identifiers for the DeFiLlama API
 var LLAMA_CHAIN_NAMES = map[uint64]string{
 	1:     `ethereum`,
 	10:    `optimism`,
 	250:   `fantom`,
 	42161: `arbitrum`,
 }
+
+// LLAMA_PRICE_URL contains the URL for the DeFiLlama pricing API
 var LLAMA_PRICE_URL = `https://coins.llama.fi/prices/current/`
 
-// GECKO_PRICE_URL contains the URL for the CoinGecko API
+// GECKO_CHAIN_NAMES contains the chain identifiers for the CoinGecko API
 var GECKO_CHAIN_NAMES = map[uint64]string{
 	1:     `ethereum`,
 	10:    `optimistic-ethereum`,
 	250:   `fantom`,
 	42161: `arbitrum-one`,
 }
+
+// GECKO_PRICE_URL contains the URL for the CoinGecko API
 var GECKO_PRICE_URL = `https://api.coingecko.com/api/v3/coins/`

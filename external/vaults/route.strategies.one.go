0997f2022a4abb3f48cf4641007c175f141ab222
borrito <30872891+kyhoon@users.noreply.github.com>
2023-02-03 23:49:56 +0100

    feat: add strategy routes (#203)
    * feat: add strategy routes
    
    * fix: change docstring
    
    * feat: remove query params for strats
    
    * refactor: remove unnecessary vars
    
    * feat: add documentation for the strategies routes

---
+++ external/vaults/route.strategies.one.go
@@ -0,0 +1,34 @@
+package vaults
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/yearn/ydaemon/common/helpers"
+	"github.com/yearn/ydaemon/internal/strategies"
+)
+
+// GetStrategy will, for a given chainID, return the strategy for a given address
+func (y Controller) GetStrategy(c *gin.Context) {
+	chainID, ok := helpers.AssertChainID(c.Param("chainID"))
+	if !ok {
+		c.String(http.StatusBadRequest, "invalid chainID")
+		return
+	}
+	address, ok := helpers.AssertAddress(c.Param("address"), chainID)
+	if !ok {
+		c.String(http.StatusBadRequest, "invalid address")
+		return
+	}
+
+	strategy, ok := strategies.FindStrategy(chainID, address.ToAddress())
+	if !ok {
+		c.String(http.StatusBadRequest, "invalid strategy")
+		return
+	}
+	// Always show details
+	newStrategy := NewStrategy().AssignTStrategy(strategy)
+	newStrategy.Risk = NewRiskScore().AssignTStrategyFromRisk(strategy.BuildRiskScore())
+
+	c.JSON(http.StatusOK, *newStrategy)
+}

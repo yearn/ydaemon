75e22bcfb352a14c7e0b1fa5c8a25498e91cef8e
Major <major@ycorpo.com>
2023-03-16 11:04:36 +0100

    fix: styCRV strat ignored

--- internal/indexer/indexer.go
+++ internal/indexer/indexer.go
@@ -2,7 +2,6 @@ package indexer
 
 import (
 	"github.com/ethereum/go-ethereum/common"
-	ethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/yearn/ydaemon/common/bigNumber"
 	"github.com/yearn/ydaemon/internal/prices"
 	"github.com/yearn/ydaemon/internal/strategies"
@@ -14,7 +13,7 @@ import (
 func PostProcessStrategies(chainID uint64) {
 	// Overwrite the st-yCRV strategy to set debtRatio to 100% and totalDebt to vault supply
 	if chainID == 1 {
-		styCRVStrategy, ok := strategies.FindStrategy(chainID, ethcommon.HexToAddress(`0xE7863292dd8eE5d215eC6D75ac00911D06E59B2d`))
+		styCRVStrategy, ok := strategies.FindStrategy(chainID, common.HexToAddress(`0xE7863292dd8eE5d215eC6D75ac00911D06E59B2d`))
 		if ok {
 			styCRVVault, ok := vaults.FindVault(chainID, styCRVStrategy.VaultAddress)
 			if ok {

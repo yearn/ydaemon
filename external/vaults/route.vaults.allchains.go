fc2d89f711bef6483fcadc83c8a7e9fb8756033b
Majorfi <major-tom.eth@pm.me>
2023-03-24 12:38:54 +0100

    feat: new migrable condition

--- external/vaults/route.vaults.allchains.go
+++ external/vaults/route.vaults.allchains.go
@@ -46,6 +46,8 @@ func (y Controller) GetAllVaultsForAllChains(c *gin.Context) {
 			continue
 		} else if migrable == `all` && !newVault.Migration.Available {
 			continue
+		} else if migrable == `ignore` && newVault.Migration.Available {
+			continue
 		}
 
 		vaultStrategies := strategies.ListStrategiesForVault(currentVault.ChainID, vaultAddress)

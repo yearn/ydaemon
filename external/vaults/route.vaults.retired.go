fedf0050ba3fbd9ee88aeb49623b15f3c5bc6970
Majorfi <major-tom.eth@pm.me>
2023-04-14 11:39:02 +0200

    feat: retired vaults route

---
+++ external/vaults/route.vaults.retired.go
@@ -0,0 +1,48 @@
+package vaults
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/yearn/ydaemon/common/env"
+	"github.com/yearn/ydaemon/common/helpers"
+	"github.com/yearn/ydaemon/common/sort"
+	"github.com/yearn/ydaemon/internal/vaults"
+)
+
+// GetRetiredVaults will, for a given chainID, return a list of all retired vaults
+func (y Controller) GetRetiredVaults(c *gin.Context) {
+	orderBy := helpers.SafeString(c.Query(`orderBy`), `details.order`)
+	orderDirection := helpers.SafeString(c.Query(`orderDirection`), `asc`)
+	chainID, ok := helpers.AssertChainID(c.Param(`chainID`))
+	if !ok {
+		c.String(http.StatusBadRequest, `invalid chainID`)
+		return
+	}
+
+	data := []TExternalVault{}
+	allVaults := vaults.ListVaults(chainID)
+	for _, currentVault := range allVaults {
+		vaultAddress := currentVault.Address
+		if helpers.Contains(env.BLACKLISTED_VAULTS[chainID], vaultAddress) {
+			continue
+		}
+
+		newVault := NewVault().AssignTVault(currentVault)
+		if !newVault.Details.Retired {
+			continue
+		}
+		if newVault.Migration.Available {
+			continue
+		}
+		if newVault.TVL.TVL < 1000 {
+			continue
+		}
+
+		newVault.Strategies = []*TStrategy{}
+		data = append(data, *newVault)
+	}
+
+	sort.SortBy(orderBy, orderDirection, data)
+	c.JSON(http.StatusOK, data)
+}

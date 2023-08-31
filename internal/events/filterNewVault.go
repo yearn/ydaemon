e25972099f60cdcabc3f39ae39c6ce05f7dde1d0
Major <major@ycorpo.com>
2023-08-31 19:03:06 +0200

    fix: ignored events

--- internal/events/filterNewVault.go
+++ internal/events/filterNewVault.go
@@ -116,7 +116,7 @@ func filterNewVaults(
 		}
 	} else if registry.Version == 3 {
 		if chainID == 8453 {
-			logs.Info(`Hello you2`, opts)
+			logs.Pretty(`Hello you2`, opts)
 		}
 		currentRegistry, _ := contracts.NewYRegistryV3Filterer(registry.Address, client)
 		if log, err := currentRegistry.FilterNewVault(opts, nil, nil); err == nil {

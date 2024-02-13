4b4f14c0978cadf8489c47c5816365a1d5f37911
Majorfi <major@ycorpo.com>
2024-02-13 11:04:46 +0100

    chore: add some eth tokens

--- common/env/chain.ethereum.go
+++ common/env/chain.ethereum.go
@@ -136,6 +136,8 @@ var ETHEREUM = TChain{
 		common.HexToAddress(`0xe3668873d944e4a949da05fc8bde419eff543882`), // yPrisma
 		common.HexToAddress(`0x69833361991ed76f9e8DBBcdf9ea1520fEbFb4a7`), // lp-Prisma
 		common.HexToAddress(`0x9a96ec9B57Fb64FbC60B423d1f4da7691Bd35079`), // Ajna
+		common.HexToAddress(`0x9Ba021B0a9b958B5E75cE9f6dff97C7eE52cb3E6`), // apxETH
+		common.HexToAddress(`0x04C154b66CB340F3Ae24111CC767e0184Ed00Cc6`), // pxETH
 	},
 	IgnoredTokens: []common.Address{
 		common.HexToAddress(`0x7AB4a7BE740131BdE216521B54ADddD672F44A05`), // nothing

5e4c4d255ef0c4ffd1fcd40cea58f2efc58e4e38
Ross <therossgalloway@gmail.com>
2025-11-11 18:01:45 -0500

    fix: missing Katana AprOracle Contract

--- common/env/chain.katana.go
+++ common/env/chain.katana.go
@@ -62,6 +62,10 @@ var KATANA = TChain{
 			Label:   `PUBLIC_ERC4626`,
 		},
 	},
+	APROracleContract: TContractData{
+		Address: common.HexToAddress(`0x1981AD9F44F2EA9aDd2dC4AD7D075c102C70aF92`),
+		Block:   2237016,
+	},
 	ExtraVaults:       []models.TVaultsFromRegistry{},
 	BlacklistedVaults: []common.Address{},
 	ExtraTokens:       []common.Address{},
